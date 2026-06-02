91a5946416000b878da04c4865a20d5e71d65eda
Travis Allen <travisallen938@gmail.com>
2026-06-01 20:52:23 -0500

    Refactor update_data method to streamline data retrieval from CSV log

--- aqs_data.py
+++ aqs_data.py
@@ -17,17 +17,18 @@ class AQSData:
         self.pm25 = None
         self.pm10 = None
 
+        self._file_pos = 0
+
+
     def update_data(self):
-        """Retrieve the latest data for the specified data type from the CSV log."""
-        last_row = "-,-,-,-,-,-,-,-,-,-,-,-"
-        with open('data_log.csv', newline='') as data_log:
-            reader = csv.reader(data_log, delimiter=',')
-            next(reader)
-
+        with open('data_log.csv', newline='') as f:
+            f.seek(self._file_pos)
+            reader = csv.reader(f)
             for row in reader:
-                last_row = row
+                self.dt,self.temp,self.humidity,self.dew_point,self.co2,self.voc_raw,self.voc_index,self.nox_raw,self.nox_index,self.pm100,self.pm25,self.pm10=row
 
-        self.dt,self.temp,self.humidity,self.dew_point,self.co2,self.voc_raw,self.voc_index,self.nox_raw,self.nox_index,self.pm100,self.pm25,self.pm10=last_row
+            self._file_pos = f.tell()
+
 
     def get_data(self, data_type, precision=0):
         """Get the latest data for the specified data type."""
