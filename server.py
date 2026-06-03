91ed8fab3355344f439dff51fe195ef3d040e010
Travis Allen <travisallen938@gmail.com>
2026-06-03 00:30:34 -0500

    ruh

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import csv
 
-from nicegui import ui, app
+from nicegui import ui
 from utils import get_y_label, aggregate_data_max, aggregate_data_avg, aggregate_time, load_data, format_value, get_relevant_data
 
 dts, temps, humiditys, dew_points, co2s, voc_raws, voc_indexs, nox_raws, nox_indexs, pm100s, pm25s, pm10s = load_data()
@@ -167,18 +167,7 @@ def index_page():
                         ui.label('PM 1.0: ').style('font-size: 125%; font-weight: 500')
                         pm10_label = ui.label('-').style('font-size: 125%')
 
-                    ui.timer(30, lambda: (get_latest_data(),
-                                        time_label.set_text(dts[-1]),
-                                        temp_label.set_text(f'{temps[-1]} C'),
-                                        rh_label.set_text(f'{humiditys[-1]}%'),
-                                        dp_label.set_text(f'{dew_points[-1]} C'),
-                                        co2_label.set_text(f'{co2s[-1]} ppm'),
-                                        voc_index_label.set_text(f'{voc_indexs[-1]} / 500'),
-                                        nox_index_label.set_text(f'{nox_indexs[-1]} / 500'),
-                                        pm100_label.set_text(f'{pm100s[-1]} μg/c^3'),
-                                        pm25_label.set_text(f'{pm25s[-1]} μg/c^3'),
-                                        pm10_label.set_text(f'{pm10s[-1]} μg/c^3'),))
-                    ui.timer(30, lambda: (get_latest_data(),
+                    ui.timer(30, lambda: (sync_missing_data(),
                                         time_label.set_text(dts[-1]),
                                         temp_label.set_text(f'{temps[-1]} C'),
                                         rh_label.set_text(f'{humiditys[-1]}%'),
