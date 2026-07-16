a8a01ff9e8822f92301306ad00a9c67db825df4f
Travis <allen@vectornav.com>
2026-07-16 16:37:43 -0500

    removed boilerplate and fixed bug in aqs_data

--- aqs_data.py
+++ aqs_data.py
@@ -25,25 +25,12 @@ class AQSData:
             f.seek(self._file_pos)
             reader = csv.reader(f)
             for row in reader:
-                self.dt,self.temp,self.humidity,self.dew_point,self.co2,self.voc_raw,self.voc_index,self.nox_raw,self.nox_index,self.pm100,self.pm25,self.pm10=row
+                self.time,self.temp,self.humidity,self.dew_point,self.co2,self.voc_raw,self.voc_index,self.nox_raw,self.nox_index,self.pm100,self.pm25,self.pm10=row
 
             self._file_pos = f.tell()
 
 
     def get_data(self, data_type, precision=0):
         """Get the latest data for the specified data type."""
-        datas = {   "time": self.dt,
-                    "temp": self.temp,
-                    "humidity": self.humidity,
-                    "dew_point": self.dew_point,
-                    "co2": self.co2,
-                    "voc_raw": self.voc_raw,
-                    "voc_index": self.voc_index,
-                    "nox_raw": self.nox_raw,
-                    "nox_index": self.nox_index,
-                    "pm10": self.pm10,
-                    "pm25": self.pm25,
-                    "pm100": self.pm100
-                }
-
-        return format_value(datas.get(data_type, datas["time"]), precision=precision)
+        datas = vars(self)
+        return format_value(datas[data_type], precision=precision)
