d669d0202003cb58464bf61c36da4e96f798d66f
Travis Allen <travisallen938@gmail.com>
2026-08-11 23:04:24 -0500

    added seek and tell to aqs_data to significantly reduce cpu time

--- aqs_data.py
+++ aqs_data.py
@@ -5,6 +5,7 @@ from utils import (format_value, calculate_heat_index,
                     calculate_dew_point, calculate_wet_bulb)
 @dataclass
 class AQSData:
+    csv_path: str = 'data_log.csv'
     dts: list = field(default_factory=list)
     temps: list = field(default_factory=list)
     humiditys: list = field(default_factory=list)
@@ -19,51 +20,57 @@ class AQSData:
     dps: list = field(default_factory=list)
     wetbulbs: list = field(default_factory=list)
     heat_indexes: list = field(default_factory=list)
+    _file_pos: int = 0
 
     def __post_init__(self):
         self.load_data()
 
-    def load_data(self):
-        with open('data_log.csv', newline='') as f:
-            reader = csv.reader(f)
-            next(reader)
-            for row in reader:
-                dt,temp,humidity,co2,voc_raw,voc_index,nox_raw,nox_index,pm100,pm25,pm10=row
-                self.dts.append(format_value(dt))
-                self.temps.append(format_value(temp, 2))
-                self.humiditys.append(format_value(humidity, 2))
-                self.co2s.append(format_value(co2))
-                self.voc_raws.append(format_value(voc_raw))
-                self.voc_indexs.append(format_value(voc_index))
-                self.nox_raws.append(format_value(nox_raw))
-                self.nox_indexs.append(format_value(nox_index))
-                self.pm100s.append(format_value(pm100))
-                self.pm25s.append(format_value(pm25))
-                self.pm10s.append(format_value(pm10))
-
-    def sync(self):
-        """Append any rows from the CSV newer than the last cached entry."""
-        last_dt = self.dt
-        if last_dt is None:
+    def _append_row(self, row: list[str]) -> None:
+        if len(row) != 11:
             return
 
-        with open('data_log.csv', newline='') as f:
-            reader = csv.reader(f)
-            next(reader)  # skip header
-            for row in reader:
-                dt = row[0]
-                if dt > last_dt:
-                    self.dts.append(dt)
-                    self.temps.append(format_value(row[1], 2))
-                    self.humiditys.append(format_value(row[2], 2))
-                    self.co2s.append(format_value(row[3]))
-                    self.voc_raws.append(format_value(row[4]))
-                    self.voc_indexs.append(format_value(row[5]))
-                    self.nox_raws.append(format_value(row[6]))
-                    self.nox_indexs.append(format_value(row[7]))
-                    self.pm100s.append(format_value(row[8]))
-                    self.pm25s.append(format_value(row[9]))
-                    self.pm10s.append(format_value(row[10]))
+        dt,temp,humidity,co2,voc_raw,voc_index,nox_raw,nox_index,pm100,pm25,pm10=row
+        self.dts.append(format_value(dt))
+        self.temps.append(format_value(temp, 2))
+        self.humiditys.append(format_value(humidity, 2))
+        self.co2s.append(format_value(co2))
+        self.voc_raws.append(format_value(voc_raw))
+        self.voc_indexs.append(format_value(voc_index))
+        self.nox_raws.append(format_value(nox_raw))
+        self.nox_indexs.append(format_value(nox_index))
+        self.pm100s.append(format_value(pm100))
+        self.pm25s.append(format_value(pm25))
+        self.pm10s.append(format_value(pm10))
+
+    def load_data(self):
+        with open(self.csv_path, newline='') as f:
+            next(f, None)  # skip header
+            for line in f:
+                row = next(csv.reader([line]))
+                self._append_row(row)
+            self._file_pos = f.tell()
+
+    def sync(self):
+        """Append only new CSV rows since the last read using seek/tell."""
+        with open(self.csv_path, newline='') as f:
+            f.seek(self._file_pos)
+
+            while True:
+                row_start = f.tell()
+                line = f.readline()
+
+                if not line:
+                    break
+
+                # Avoid consuming a partially written trailing line.
+                if not line.endswith('\n'):
+                    f.seek(row_start)
+                    break
+
+                row = next(csv.reader([line]))
+                self._append_row(row)
+
+            self._file_pos = f.tell()
 
     @property
     def dt(self) -> str|None:
